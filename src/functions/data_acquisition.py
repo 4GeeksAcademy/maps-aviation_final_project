abf98db592be9e2dab20c6ea0e60766267241a4f
George Perdrizet <george@perdrizet.org>
2025-04-22 00:35:18 +0000

    Added functions to download on-time flight data.

--- src/functions/data_acquisition.py
+++ src/functions/data_acquisition.py
@@ -4,6 +4,7 @@ import io
 import zipfile
 
 import requests
+from bs4 import BeautifulSoup, SoupStrainer
 import pandas as pd
 from access_parser import AccessParser
 
@@ -29,3 +30,32 @@ def parse_mdb(file:str) -> None:
     # Convert to dataframe and save as csv
     table_df=pd.DataFrame.from_dict(table)
     table_df.to_csv('../data/raw/aircraft.csv', index=False)
+
+
+def get_ontime_links(url:str) -> list:
+    '''Uses requests and beautifulsoup to parse download links for on-time
+    performance from bts.gov site'''
+
+    response=requests.get(url)
+    html_content=response.text
+    soup=BeautifulSoup(html_content, 'html.parser', parse_only=SoupStrainer('a'))
+    
+    links=[]
+
+    for link in soup:
+        if link.has_attr('href'):
+            link_text=link['href']
+            if link_text.split('.')[-1] == 'zip':
+                links.append(link_text)
+
+    return links
+
+
+def download_ontime_data(links:list) -> None:
+    '''Takes list of data download urls, download the files to disk.'''
+
+    for link in links:
+        complete_link=f'https://www.bts.dot.gov/{link}'
+        response=requests.get(complete_link, timeout=10)
+        archive=zipfile.ZipFile(io.BytesIO(response.content))
+        archive.extractall('../data/raw/')
